8768d982a335e3c2dd73ce87ec00b29dbf7188ec
dmmontero <danny.montero@gmail.com>
2018-11-09 16:45:38 -0500

    Practicas Ruby

---
+++ calc.rb
@@ -0,0 +1,28 @@
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+puts 9.0 / 2
+
+
+puts "2" * 5
+
+
+puts "Mi nombre es Jo'C."
+puts '¡Mi sobrenombre es "Pepe"!'
+
+puts "Hola" * 4
+
+
+name = 'Patricia Rosanna Jessica Mildred Oppenheimer'
+puts 'Me llamo ' + name + '.'
+puts 'Wow!  "' + name + '" es un nombre realmente largo!'
+
+var1 = 8
+var2 = var1
+puts var1
+puts var2
+var1 = 10;
+puts var1
+puts var2
+
---
+++ normas.rb
@@ -0,0 +1,4 @@
+puts 'Soy una clases = ' + self.class.to_s  
+puts 'Soy un objeto = ' + self.to_s  
+print 'Los métodos del objeto son = '  
+puts self.private_methods.sort
