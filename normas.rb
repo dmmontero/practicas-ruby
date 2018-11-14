57cb1f4910c711381c3792031980f8672a6bfbcc
dmmontero <danny.montero@gmail.com>
2018-11-14 17:12:44 -0500

    Prácticas tipos de variable

--- normas.rb
+++ normas.rb
@@ -1,4 +1,13 @@
-puts 'Soy una clases = ' + self.class.to_s  
-puts 'Soy un objeto = ' + self.to_s  
-print 'Los métodos del objeto son = '  
-puts self.private_methods.sort
+# puts 'Soy una clase = ' + self.class.to_s  
+# puts 'Soy un objeto = ' + self.to_s  
+# print 'Los métodos del objeto son = '  
+# puts self.private_methods.sort
+vara = 45
+puts "El valor de vara es #{vara}"
+puts "valor variable $: #{$$}"
+
+
+hsh = { "red" => 0xf00, "green" => 0x0f0, "blue" => 0x00f }
+hsh.each do |key, value|
+   print key, " is ", value, "\n"
+end
